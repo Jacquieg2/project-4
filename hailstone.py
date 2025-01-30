b853d0a73789a914e5a2013e2b0395f209c582d6
Jacquieg2 <garcija4@oregonstate.edu>
2025-01-29 19:16:48 -0500

    Update hailstone.py

--- hailstone.py
+++ hailstone.py
@@ -2,23 +2,24 @@
 # GitHub username: Jgarcia2
 # Date: 01/28/2025
 # Description: Hailstone
-def fall_distance(time):
+def hailstone(n):
     """
-    Calculate the distance an object falls due to gravity in a given time.
-
-    The formula used is:
-        d = (1/2) * g * t^2
-    where:
-        d = distance in meters,
-        g = 9.8 m/s^2 (acceleration due to gravity),
-        t = time in seconds.
+    Compute the number of steps required to reach 1 in the hailstone sequence.
 
     Parameters:
-    time (float): The time in seconds the object has been falling.
+    n (int): A positive integer, the starting number of the sequence.
 
     Returns:
-    float: The distance in meters the object has fallen.
+    int: The number of steps required to reach 1.
     """
-    g = 9.8  # Acceleration due to gravity in m/s^2
-    distance = 0.5 * g * time ** 2
-    return distance
+    steps = 0
+
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        steps += 1
+
+    return steps
+
